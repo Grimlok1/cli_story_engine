4fe5e291c7299d2711ff9d873e10cd3460b2311a
Milo komulainen <Grimlok1@users.noreply.github.com>
2026-01-05 20:26:13 +0200

    Add files via upload
    Update to the example_story file

---
+++ text.py
@@ -0,0 +1,24 @@
+START = """Markus haluaa lähteä Joosepin luo kyläilemään, mutta ilmanlämpötila on laskenut -30 asteeseen.
+Miten Markusksen pitäsi pukeutua, jotta hän selviää viiden kilometrin kävely matkasta paleltumatta?
+"""
+
+VILLAHOUSUT="""Pääset noin 500 metrin matkan kotoasi kunnes huomaat, että villahousut ovat avainliian kuumat.
+Mitään ei ole enää tehtävissä. Tunnet kuinka kehosi peittyy kautaalta hiestä!
+Hävisit pelin!"""
+
+KALSARIT= """Olet melkein ystäväsi Joosepin luona kunnes tunnet jaloissasi kuumuuden tunnetta.
+Voi ei! Olet pian yltäpäät hien peitossa. Mitä teet?"""
+
+RIISU_KALSARIT= """Vedät kalsarit nopeasti pois päältäsi. Huh helpotus!
+Voi ei, Naapurit huomasivat kuin riisuit kalsarisi keskellä tietä. Kuinka noloa!
+Hävisit pelin!
+"""
+
+HYPPÄÄ_LUMEEN= """Hypäät nopeasti lumihankeen ennekuin ylikuumenet.
+Mikä helpotus! Pelastuit ylikuumenemiselta ja pääsit Joosepin luo.
+Voitit pelin
+"""
+
+JUOKSE="""Yrität juosta Ystäväsi Joosepin luon ennenkuin olet yltäpäältä hien peitossa.
+Äkillinen liikunta kuitenkin vain pahentaa tilannetta ja huomaat olevasi nahkean hikinen ennekuin pääset Joosepin ovelle.
+Hävisit pelin!"""
